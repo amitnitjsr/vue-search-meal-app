3077b240348989d5a7cbefdda2fc5fdf34a028a1
amitnitjsr <amitnips007@gmail.com>
2023-06-24 16:17:19 +0530

    mealItem

--- src/components/GuestLayout.vue
+++ src/components/GuestLayout.vue
@@ -3,5 +3,5 @@
 </template>
 
 <script setup>
-// import Navbar from '../components/Navbar.vue'
+
 </script>
--- src/components/MealItem.vue
+++ src/components/MealItem.vue
@@ -1,20 +1,24 @@
 <template>
- <div 
-    class="bg-white shadow rounded-xl"
-    v-for="meal of meals" 
-    :key="meal.idMeal"
-    >
-    <router-link :to="{name: 'mealDetails', params: {id: meal.idMeal}}">
-        <img :src="meal.strMealThumb" :alt="meal.strMeal" 
-        class="rounded-t-xl h-48 w-full object-cover"/>
+  <div
+    class="bg-white shadow rounded-xl hover:scale-105 transition-all"
+  >
+    <router-link :to="{ name: 'mealDetails', params: { id: meal.idMeal } }">
+      <img
+        :src="meal.strMealThumb"
+        :alt="meal.strMeal"
+        class="rounded-t-xl w-full h-48 object-cover"
+      />
     </router-link>
-        <h3 class="p-3 font-semibold">{{ meal.strMeal }}</h3>
-        <div class="flex items-center justify-between">
-            <YouTubeButton :href="meal.strYoutube">
-                YouTube
-            </YouTubeButton>
-        </div>
+    <div class="p-3">
+      <h3 class="font-bold">{{ meal.strMeal }}</h3>
+      <p class="mb-4">
+        {{ $filters.truncateWords(meal.strInstructions, 20) }}
+      </p>
+      <div class="flex items-center justify-between">
+        <YouTubeButton :href="meal.strYoutube" />
+      </div>
     </div>
+  </div>
 </template>
 
 <script setup>
