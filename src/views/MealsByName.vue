d125598ebc074bcb2f894fae1f7482e616224dfe
amitnitjsr <amitnips007@gmail.com>
2023-06-24 16:07:15 +0530

    mealsByName

--- src/views/MealsByName.vue
+++ src/views/MealsByName.vue
@@ -1,24 +1,29 @@
 <template>
 <div class="p-8 pb-0">
-    <input type="text" class="rounded border-2 bg-white border-grey-200 w-full" 
-        placeholder="Search for Meals"
-        @change="searchMeals"
+    <h1 class="text-4xl font-bold mb-4 text-orange-500">Search Meals by Name</h1>
+</div>
+<div class="px-8 pb-3">
+    <input
+      type="text"
+      v-model="keyword"
+      class="rounded border-2 bg-white border-gray-200 focus:ring-orange-500 focus:border-orange-500 w-full"
+      placeholder="Search for Meals"
+      @change="searchMeals"
     />
 </div>
+
 <Meals :meals="meals" />
 </template>
 
 <script setup>
-import {computed, onMounted, ref} from 'vue';
-import axiosClient from '../axiosClient';
-import store from '../store';
-import YouTubeButton from '../components/YouTubeButton.vue';
-import MealItem from '../components/MealItem.vue';
-import Meals from '../components/Meals.vue';
-import {useRoute} from 'vue-router';
+import { computed } from "@vue/reactivity";
+import { onMounted, ref } from "vue";
+import { useRoute } from "vue-router";
+import store from "../store";
+import Meals from '../components/Meals.vue'
 
 const route = useRoute();
-const keyword = ref('');
+const keyword = ref("");
 const meals = computed(() => store.state.searchedMeals);
 
 function searchMeals() {
