dd483c37f24b6e3d6d2bc746eb1622300a81eb6e
amitnitjsr <amitnips007@gmail.com>
2023-06-24 16:02:09 +0530

    mealsByIngredient

--- src/views/MealsByIngredient.vue
+++ src/views/MealsByIngredient.vue
@@ -1,37 +1,23 @@
 <template>
-<!-- <div class="p-8">
-    <h1 class="text-4xl font-bold mb-4">Ingredients</h1>
-    <router-link 
-    :to="{
-        name: 'byIngredient',
-        params: { ingredient: ingredient.idIngredient }
-    }"
-    v-for="ingredient of ingredients"
-    :key="ingredient.idIngredient"
-    class="bg-white rounded p-3 mb-3 shadow"
-    >
-        <h3 class="text-2x font-bold">
-            {{ ingredient.strIngredient }}
-        </h3>
-        <p>
-            {{ ingredient.strDescription }}
-        </p>
-    </router-link>
-</div> -->
+<div class="p-8 pb-0">
+    <h1 class="text-4xl font-bold mb-4 text-orange-500">Meals for {{ ingredient.strIngredient }}</h1>
+</div>
+ 
 <Meals :meals="meals" />
 </template>
 
 <script setup>
-import { computed } from 'vue';
-import { onMounted } from 'vue';
-import store from '../store';
-import { useRoute } from 'vue-router';
-import Meals from '../components/Meals.vue';
+import { computed } from "@vue/reactivity";
+import { onMounted } from "vue";
+import { useRoute } from "vue-router";
+import store from "../store";
+import Meals from '../components/Meals.vue'
 
 const route = useRoute();
-const meals = computed(() => store.state.mealsByIngredient);
+const ingredient = computed(() => store.state.ingredient)
+const meals = computed(() => store.state.mealsByIngredient)
 
 onMounted(() => {
-   store.dispatch('searchMealsByIngredient', route.params.ingredient)
-});
+  store.dispatch('searchMealsByIngredient', route.params.ingredient)
+})
 </script>
